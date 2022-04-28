6650555539232b15177ca451d42eb9b38a0f4d59
DmitryP0l <dmitrypoletaeu@gmail.com>
2022-04-28 15:13:12 +0300

    Initial Commit

---
+++ VKNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKNews
+//
+//  Created by lion on 28.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
